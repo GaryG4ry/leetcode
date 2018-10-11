d536e5f6826383da5442ee9da748056161f8c1a3
Gary <591847680@qq.com>
2018-10-11 20:24:05 +0800

    valid kuohao

--- 有效的括号.py
+++ 有效的括号.py
@@ -4,11 +4,19 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-        dic = {'{': '}', '(': ')', '[': ']'}
+
+        dic = {'}': '{', ')': '(', ']': '['}
         temp = []
         length = len(s)
+        if length == 0 or length % 2 != 0:
+            return False
         for i in range(length):
-            pass
+            if s[i] in dic.values():
+                temp.append(s[i])
+            else:
+                if not temp or temp.pop() != dic[s[i]]:
+                    return False
+        return True
 
 
-Solution().isValid('{}{}ASd')
+print(Solution().isValid(''))
