1d8f53eb2b7169b54f1ce5c956b123f7d9420c2a
Gary <591847680@qq.com>
2018-10-11 20:22:59 +0800

    merge two ordered list

---
+++ 合并两个链表.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # add one temp node zero
+        newList = ListNode(0)
+        node = newList
+        while l1 and l2:
+            if l1.val < l2.val:
+                node.next = l1
+                l1 = l1.next
+            else:
+                node.next = l2
+                l2 = l2.next
+            node = node.next
+        if l1:
+            node.next = l1
+        elif l2:
+            node.next = l2
+        return newList.next
+
+
+"""
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+"""
