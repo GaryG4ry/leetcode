71c0225b574156b50cc1ea715dedbca45e64b2e9
Gary <591847680@qq.com>
2018-10-11 20:21:43 +0800

    10.10

--- 有效的括号.py
+++ 有效的括号.py
@@ -4,16 +4,11 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-        if len(s) % 2 != 0 or len(s) == 0:
-            return False
-        for i in range(len(s)//2):
-            if s[i] == s[len(s) - i - 1]:
-                continue
-            else:
-                return False
-        return True
+        dic = {'{': '}', '(': ')', '[': ']'}
+        temp = []
+        length = len(s)
+        for i in range(length):
+            pass
 
 
-x = Solution()
-print(x.isValid(''))
-print(len(''))
+Solution().isValid('{}{}ASd')
